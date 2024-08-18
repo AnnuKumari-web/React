cf96d77b5755e840cc164eb33482956f5cbe4fa1
Annu <annukmri.ak@gmail.com>
2024-08-18 20:56:19 +0530

    add react basic projects

---
+++ 01basicreact/src/Demo.js
@@ -0,0 +1,7 @@
+function Demo(){
+    return(
+        <h2>Demo is ready to be rendered.</h2>
+    )
+}
+
+export default Demo;
---
+++ 01viteReact/src/Demo.jsx
@@ -0,0 +1,7 @@
+function Demo(){
+    return(
+        <h3>Demo is ready</h3>
+    )
+}
+
+export default Demo;
